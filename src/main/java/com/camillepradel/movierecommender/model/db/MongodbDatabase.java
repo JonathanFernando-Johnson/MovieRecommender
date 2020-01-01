2fdafe113ad92f0652a431d9b43556fa53e5d11a
Jonathan Fernando-Johnson <jonathanfj95@gmail.com>
2020-01-01 19:31:12 +0100

    getAllMovies()

--- src/main/java/com/camillepradel/movierecommender/model/db/MongodbDatabase.java
+++ src/main/java/com/camillepradel/movierecommender/model/db/MongodbDatabase.java
@@ -3,28 +3,94 @@ package com.camillepradel.movierecommender.model.db;
 import com.camillepradel.movierecommender.model.Genre;
 import com.camillepradel.movierecommender.model.Movie;
 import com.camillepradel.movierecommender.model.Rating;
+import com.mongodb.*;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 public class MongodbDatabase extends AbstractDatabase {
+    DB db = null;
+
+    public MongodbDatabase () {
+        try {
+            /* Connect to MongoDB */
+            MongoClient mongo = new MongoClient("localhost", 27017);
+
+            /* Get database */
+            this.db = mongo.getDB("MovieRecommenderDB");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Genre> getListGenre (int movieID) {
+        List<Genre> genreList = new ArrayList<Genre>();
+        if (this.db != null) {
+            try {
+                DBCollection mov_genreCollection = this.db.getCollection("mov_genre");
+                DBCollection genresCollection = this.db.getCollection("genres");
+
+                //Recupere les genre de movieID
+                BasicDBObject mov_genreFilter = new BasicDBObject();
+                mov_genreFilter.put("mov_id", movieID);
+                DBCursor cursor = mov_genreCollection.find(mov_genreFilter);
+
+                while (cursor.hasNext()) {
+                    DBObject mov_genre = cursor.next();
+
+                    //Recupere l'id du genre
+                    BasicDBObject genreFilter = new BasicDBObject();
+                    genreFilter.put("id", Integer.parseInt(mov_genre.get("genre").toString()));
+                    DBObject currentGenre = genresCollection.findOne(genreFilter);
+
+                    genreList.add(new Genre(Integer.parseInt(currentGenre.get("id").toString()), currentGenre.get("name").toString()));
+                }
+                return genreList;
+            } catch (Exception e) {
+                System.out.println("Erreur lors de la creation de la liste des genre du movie : " + movieID);
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    public Movie generateMovie (DBObject currentMovie) {
+        try {
+            int movieId = Integer.parseInt(currentMovie.get("id").toString());
+            String movieTitle = currentMovie.get("title").toString();
+            List<Genre> listeGenre = this.getListGenre(movieId);
+            Movie movie = new Movie(movieId, movieTitle, listeGenre);
+            return movie;
+        } catch (NumberFormatException e) {
+            System.out.println("Erreur création film lors du parseInt");
+            e.printStackTrace();
+        }
+        return null;
+    }
 
     @Override
-    public List<Movie> getAllMovies() {
-        // TODO: write query to retrieve all movies from DB
+    public List<Movie> getAllMovies () {
         List<Movie> movies = new LinkedList<Movie>();
-        Genre genre0 = new Genre(0, "genre0");
-        Genre genre1 = new Genre(1, "genre1");
-        Genre genre2 = new Genre(2, "genre2");
-        movies.add(new Movie(0, "Titre 0", Arrays.asList(new Genre[]{genre0, genre1})));
-        movies.add(new Movie(1, "Titre 1", Arrays.asList(new Genre[]{genre0, genre2})));
-        movies.add(new Movie(2, "Titre 2", Arrays.asList(new Genre[]{genre1})));
-        movies.add(new Movie(3, "Titre 3", Arrays.asList(new Genre[]{genre0, genre1, genre2})));
+        if (this.db != null) {
+            try {
+                DBCollection moviesCollection = this.db.getCollection("movies");
+                DBCursor cursor = moviesCollection.find();
+                while (cursor.hasNext()) {
+                    DBObject currentMovie = cursor.next();
+                    movies.add(this.generateMovie(currentMovie));
+                }
+            } catch (Exception e) {
+                System.out.println("Erreur getAllMovies()");
+                e.printStackTrace();
+            }
+        }
         return movies;
     }
 
     @Override
-    public List<Movie> getMoviesRatedByUser(int userId) {
+    public List<Movie> getMoviesRatedByUser (int userId) {
         // TODO: write query to retrieve all movies rated by user with id userId
         List<Movie> movies = new LinkedList<Movie>();
         Genre genre0 = new Genre(0, "genre0");
@@ -36,7 +102,7 @@ public class MongodbDatabase extends AbstractDatabase {
     }
 
     @Override
-    public List<Rating> getRatingsFromUser(int userId) {
+    public List<Rating> getRatingsFromUser (int userId) {
         // TODO: write query to retrieve all ratings from user with id userId
         List<Rating> ratings = new LinkedList<Rating>();
         Genre genre0 = new Genre(0, "genre0");
@@ -47,14 +113,14 @@ public class MongodbDatabase extends AbstractDatabase {
     }
 
     @Override
-    public void addOrUpdateRating(Rating rating) {
+    public void addOrUpdateRating (Rating rating) {
         // TODO: add query which
         //         - add rating between specified user and movie if it doesn't exist
         //         - update it if it does exist
     }
 
     @Override
-    public List<Rating> processRecommendationsForUser(int userId, int processingMode) {
+    public List<Rating> processRecommendationsForUser (int userId, int processingMode) {
         // TODO: process recommendations for specified user exploiting other users ratings
         //       use different methods depending on processingMode parameter
         Genre genre0 = new Genre(0, "genre0");
@@ -76,5 +142,5 @@ public class MongodbDatabase extends AbstractDatabase {
         recommendations.add(new Rating(new Movie(2, titlePrefix + "Titre 2", Arrays.asList(new Genre[]{genre1})), userId, 4));
         recommendations.add(new Rating(new Movie(3, titlePrefix + "Titre 3", Arrays.asList(new Genre[]{genre0, genre1, genre2})), userId, 3));
         return recommendations;
-    }    
+    }
 }
