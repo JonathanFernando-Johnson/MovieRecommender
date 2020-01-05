621a0617590e206786881aff54add7089e2d9980
Jonathan Fernando-Johnson <jonathanfj95@gmail.com>
2020-01-05 13:05:17 +0100

    Modif pom et Neo4jDatabase

--- src/main/java/com/camillepradel/movierecommender/model/db/Neo4jDatabase.java
+++ src/main/java/com/camillepradel/movierecommender/model/db/Neo4jDatabase.java
@@ -3,78 +3,148 @@ package com.camillepradel.movierecommender.model.db;
 import com.camillepradel.movierecommender.model.Genre;
 import com.camillepradel.movierecommender.model.Movie;
 import com.camillepradel.movierecommender.model.Rating;
-import java.util.Arrays;
-import java.util.LinkedList;
+
 import java.util.List;
+import java.util.Map;
+
+import org.neo4j.driver.v1.*;
 
 public class Neo4jDatabase extends AbstractDatabase {
+    // db connection info
+    String url = "bolt://localhost:7687";
+    //	            + "?zeroDateTimeBehavior=convertToNull&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    String login = "neo4j";
+    String password = "root";
+    Driver driver;
 
-    @Override
-    public List<Movie> getAllMovies() {
-        // TODO: write query to retrieve all movies from DB
-        List<Movie> movies = new LinkedList<Movie>();
-        Genre genre0 = new Genre(0, "genre0");
-        Genre genre1 = new Genre(1, "genre1");
-        Genre genre2 = new Genre(2, "genre2");
-        movies.add(new Movie(0, "Titre 0", Arrays.asList(new Genre[]{genre0, genre1})));
-        movies.add(new Movie(1, "Titre 1", Arrays.asList(new Genre[]{genre0, genre2})));
-        movies.add(new Movie(2, "Titre 2", Arrays.asList(new Genre[]{genre1})));
-        movies.add(new Movie(3, "Titre 3", Arrays.asList(new Genre[]{genre0, genre1, genre2})));
-        return movies;
+    public Neo4jDatabase () {
+        System.out.println("init connection");
+		this.driver = GraphDatabase.driver(this.url, AuthTokens.basic(this.login, this.password));
     }
 
     @Override
-    public List<Movie> getMoviesRatedByUser(int userId) {
-        // TODO: write query to retrieve all movies rated by user with id userId
-        List<Movie> movies = new LinkedList<Movie>();
-        Genre genre0 = new Genre(0, "genre0");
-        Genre genre1 = new Genre(1, "genre1");
-        Genre genre2 = new Genre(2, "genre2");
-        movies.add(new Movie(0, "Titre 0", Arrays.asList(new Genre[]{genre0, genre1})));
-        movies.add(new Movie(3, "Titre 3", Arrays.asList(new Genre[]{genre0, genre1, genre2})));
-        return movies;
-    }
+    public List<Movie> getAllMovies () {
+        try (Session session = this.driver.session()) {
+            final String statement = "MATCH (m:Movie)-[:CATEGORIZED_AS]->(g:Genre) RETURN m, collect(g) as genres";
+            return session.readTransaction(tx -> tx.run(statement, Map.of())).list(record -> {
+                Value movieNode = record.get("m");
+                List<Genre> genres = record.get("genres")
+                        .asList(node -> new Genre(node.get("id").asInt(), node.get("name").asString()));
 
-    @Override
-    public List<Rating> getRatingsFromUser(int userId) {
-        // TODO: write query to retrieve all ratings from user with id userId
-        List<Rating> ratings = new LinkedList<Rating>();
-        Genre genre0 = new Genre(0, "genre0");
-        Genre genre1 = new Genre(1, "genre1");
-        ratings.add(new Rating(new Movie(0, "Titre 0", Arrays.asList(new Genre[]{genre0, genre1})), userId, 3));
-        ratings.add(new Rating(new Movie(2, "Titre 2", Arrays.asList(new Genre[]{genre1})), userId, 4));
-        return ratings;
-    }
-
-    @Override
-    public void addOrUpdateRating(Rating rating) {
-        // TODO: add query which
-        //         - add rating between specified user and movie if it doesn't exist
-        //         - update it if it does exist
-    }
-
-    @Override
-    public List<Rating> processRecommendationsForUser(int userId, int processingMode) {
-        // TODO: process recommendations for specified user exploiting other users ratings
-        //       use different methods depending on processingMode parameter
-        Genre genre0 = new Genre(0, "genre0");
-        Genre genre1 = new Genre(1, "genre1");
-        Genre genre2 = new Genre(2, "genre2");
-        List<Rating> recommendations = new LinkedList<Rating>();
-        String titlePrefix;
-        if (processingMode == 0) {
-            titlePrefix = "0_";
-        } else if (processingMode == 1) {
-            titlePrefix = "1_";
-        } else if (processingMode == 2) {
-            titlePrefix = "2_";
-        } else {
-            titlePrefix = "default_";
+                return new Movie(movieNode.get("id").asInt(), movieNode.get("title").asString(), genres);
+            });
         }
-        recommendations.add(new Rating(new Movie(0, titlePrefix + "Titre 0", Arrays.asList(new Genre[]{genre0, genre1})), userId, 5));
-        recommendations.add(new Rating(new Movie(1, titlePrefix + "Titre 1", Arrays.asList(new Genre[]{genre0, genre2})), userId, 5));
-        recommendations.add(new Rating(new Movie(2, titlePrefix + "Titre 2", Arrays.asList(new Genre[]{genre1})), userId, 4));
-        recommendations.add(new Rating(new Movie(3, titlePrefix + "Titre 3", Arrays.asList(new Genre[]{genre0, genre1, genre2})), userId, 3));
-        return recommendations;
+    }
+
+    @Override
+    public List<Movie> getMoviesRatedByUser (int userId) {
+        try (Session session = this.driver.session()) {
+            String statement = "MATCH (u:User {id:" + userId
+                    + "})-[r]->(m:Movie) -[:CATEGORIZED_AS]->(g:Genre) WHERE TYPE(r) = \"RATED\" RETURN m, collect(g) as genres";
+            return session.readTransaction(tx -> tx.run(statement, Map.of())).list(record -> {
+                Value movieNode = record.get("m");
+                List<Genre> genres = record.get("genres")
+                        .asList(node -> new Genre(node.get("id").asInt(), node.get("name").asString()));
+
+                return new Movie(movieNode.get("id").asInt(), movieNode.get("title").asString(), genres);
+            });
+        }
+    }
+
+    @Override
+    public List<Rating> getRatingsFromUser (int userId) {
+        try (Session session = this.driver.session()) {
+            String statement = "MATCH (u:User {id:" + userId
+                    + "})-[r]->(m:Movie) -[:CATEGORIZED_AS]->(g:Genre) WHERE TYPE(r) = \"RATED\" RETURN m, r, collect(g) as genres";
+            return session.readTransaction(tx -> tx.run(statement, Map.of())).list(record -> {
+                Value movieNode = record.get("m");
+                Value ratedNode = record.get("r");
+                List<Genre> genres = record.get("genres")
+                        .asList(node -> new Genre(node.get("id").asInt(), node.get("name").asString()));
+                Movie movie = new Movie(movieNode.get("id").asInt(), movieNode.get("title").asString(), genres);
+                return new Rating(movie, userId, ratedNode.get("note").asInt());
+            });
+        }
+    }
+
+    @Override
+    public void addOrUpdateRating (Rating rating) {
+        try (Session session = this.driver.session()) {
+            String statement = "MATCH (u:User {id:" + rating.getUserId() + "}),(m:Movie {id:" + rating.getMovieId()
+                    + "}) " + "MERGE (u)-[r:RATED]->(m)" + "ON MATCH SET r = {note:" + rating.getScore() + ",timestamp:"
+                    + System.currentTimeMillis() + "}" + "ON CREATE SET r = {note:" + rating.getScore() + ",timestamp:"
+                    + System.currentTimeMillis() + "}" + "RETURN r";
+            System.out.println(statement);
+            session.readTransaction(tx -> tx.run(statement));
+        }
+        // String statement = "MATCH (u:User {id:"+
+        // rating.getUserId()+"})-[r:RATED]->(m:Movie {id:"+ rating.getMovieId()+"}) SET
+        // r = {note:"+rating.getScore()+",timestamp:"+ System.currentTimeMillis()+"}
+        // RETURN r";
+    }
+
+    @Override
+    public List<Rating> processRecommendationsForUser (int userId, int processingMode) {
+        try (Session session = this.driver.session()) {
+
+            String statement = this.getStatementForRecom(userId, processingMode);
+            return session.readTransaction(tx -> tx.run(statement, Map.of())).list(record -> {
+                Value movieNode = record.get("movie");
+                Value ratedNode = record.get("rated");
+                List<Genre> genres = null;
+                Movie movie = new Movie(movieNode.get("id").asInt(), movieNode.get("title").asString(), genres);
+                return new Rating(movie, record.get("watched_by").asInt(), ratedNode.asInt());
+            });
+        }
+        // TODO: process recommendations for specified user exploiting other users
+        // ratings
+        // use different methods depending on processingMode parameter
+
+    }
+
+    public String getStatementForRecom (int userId, int processingMode) {
+        String statement = "";
+        switch (processingMode) {
+            case 1:
+                statement = "MATCH (target_user:User {id :" + userId + "})-[:RATED]->(m:Movie) <-[:RATED]-(other_user:User) " +
+                        "WITH other_user, count(distinct m.title) AS num_common_movies, target_user " +
+                        "ORDER BY num_common_movies DESC " +
+                        "LIMIT 1 " +
+                        "MATCH (other_user)-[rat_other_user:RATED]->(m2:Movie) " +
+                        "WHERE NOT ((target_user)-[:RATED]->(m2)) " +
+                        "RETURN m2 AS movie, rat_other_user AS rated, other_user.id AS watched_by " +
+                        "ORDER BY rat_other_user.note DESC ";
+                break;
+            case 2:
+                statement = "MATCH (target_user:User {id :" + userId + "})-[:RATED]->(m:Movie) <-[:RATED]-(other_user:User) " +
+                        "WITH other_user, count(distinct m.title) AS num_common_movies, target_user " +
+                        "ORDER BY num_common_movies DESC " +
+                        "LIMIT 5 " +
+                        "MATCH (other_user)-[rat_other_user:RATED]->(m2:Movie) " +
+                        "WHERE NOT ((target_user)-[:RATED]->(m2)) " +
+                        "RETURN m2 AS movie, toInteger(avg(rat_other_user.note)) AS rated, count(other_user.id) AS watched_by " +
+                        "ORDER BY rated DESC, watched_by DESC";
+                break;
+            case 3:
+                statement = "MATCH (target_user:User {id :" + userId + "})-[r1:RATED]->(m:Movie) <-[r2:RATED]-(other_user:User) " +
+                        "WITH other_user, count(distinct m.title) AS num_common_movies, target_user,  (4-abs(r1.note - r2.note)) as score" +
+                        "ORDER BY num_common_movies DESC " +
+                        "LIMIT 5 " +
+                        "MATCH (other_user)-[rat_other_user:RATED]->(m2:Movie) " +
+                        "WHERE NOT ((target_user)-[:RATED]->(m2)) " +
+                        "RETURN m2 AS movie, toInteger(avg(rat_other_user.note)) AS rated, count(other_user.id) AS watched_by, avg(score) " +
+                        "ORDER BY avg(score) desc, rated DESC, watched_by DESC";
+            default:
+                statement = "MATCH (target_user:User {id :" + userId + "})-[:RATED]->(m:Movie) <-[:RATED]-(other_user:User) " +
+                        "WITH other_user, count(distinct m.title) AS num_common_movies, target_user " +
+                        "ORDER BY num_common_movies DESC " +
+                        "LIMIT 1 " +
+                        "MATCH (other_user)-[rat_other_user:RATED]->(m2:Movie) " +
+                        "WHERE NOT ((target_user)-[:RATED]->(m2)) " +
+                        "RETURN m2 AS movie, rat_other_user AS rated, other_user.id AS watched_by " +
+                        "ORDER BY rat_other_user.note DESC ";
+                break;
+        }
+        return statement;
     }
 }
